aea81ad2c5ad90c880c8c28c3d89b5c357188941
Kork <hail3yp@gmail.com>
2024-02-01 20:00:17 -0500

    creating routes, models, db, config, and seeds folders. Created server.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const tagData = Product.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.post('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tagData = Product.findAll({
+      where: {id: req.params.id},
+      include: [{model: Product}]
+    })
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try{
+    Product.create(req.body)
+    .then((product) => {
+      
+    })
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
